ece2f5b54a5d6792e2a1b6e9b7b5b5652756ed61
Maggie Summers <summers.maggie@gmail.com>
2018-01-26 13:35:13 -0800

    add checkbox functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,15 @@
 //biz logic
-function Pizza(size, topping1, topping2) {
+function Pizza(size, toppings) {
   this.size = size;
-  this.topping1 = topping1;
-  this.topping2 = topping2;
+  this.toppings = [];
 };
 
 Pizza.prototype.price = function() {
-  if (this.size === "small" && this.topping1 !== "none" && this.topping2 === "none") {
-    return (7 + 2 + 0);
-  } else if (this.size === "small" && this.topping1 === "none" && this.topping2 === "none") {
-    return (7 + 2 + 2);
-  } else if (this.size === "medium" && this.topping1 !== "none" && this.topping2 === "none") {
-    return (9 + 2 + 0);
-  } else if (this.size === "medium" && this.topping1 === "none" && this.topping2 === "none") {
-    return (9 + 2 + 2);
-  } else if (this.size === "large" && this.topping1 !== "none" && this.topping2 === "none") {
-    return (11 + 2 + 0)
-  } else if (this.size === "large" && this.topping1 === "none" && this.topping2 === "none") {
-    return (11 + 2 + 2)
+  if (this.size === "small" && this.toppings === 3) {
+    return (7 + 6);
+  // } else if (this.size === "small" && this.topping1 === "none" && this.topping2 === "none") {
+  //   return (7 + 2 + 2);
+
   } else {
     return ("Please try again.");
   }
@@ -25,21 +17,19 @@ Pizza.prototype.price = function() {
 
 //ui logic
 $(document).ready(function() {
+  var pizzaOne = new Pizza(this.size, this.toppings);
+  var toppings = [];
+
   $("button#order").click(function() {
-
     var inputtedSize = $("input:radio[name=size]:checked").val();
-    var inputtedTopping1 = $("input:radio[name=topping1]:checked").val();
-    var inputtedTopping2 = $("input:radio[name=topping2]:checked").val();
+    alert(inputtedSize);
 
-    var pizzaOne = new Pizza(inputtedSize, inputtedTopping1, inputtedTopping2);
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      var inputtedToppings = $(this).val();
+      toppings.push(inputtedToppings);
+      alert(toppings);
+    });
 
     $("#price").text(pizzaOne.price());
-
-    // newPizza.price();
-    // $("li#price").text(this.size);
-    // var topping2 = $("input:radio[name=topping2]:checked").val();
-    // var topping3 = $("input:radio[name=topping3]:checked").val();
-    // var topping4 = $("input:radio[name=topping4]:checked").val()
-
   });
 });

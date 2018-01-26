6eda66a6501f406e1557048b72d1e0974e628c34
Maggie Summers <summers.maggie@gmail.com>
2018-01-26 11:13:17 -0800

    add js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,37 @@
 //biz logic
-function Pizza(size) {
+function Pizza(size, topping) {
   this.size = size;
-  this.topping = [];
+  this.topping = topping;
 };
 
-Pizza.prototype.price = function () {
-  if (this.size === small) {
-    return (this.size + (2 * this.topping));
+Pizza.prototype.price = function() {
+  if (this.size === "small") {
+    return ("9");
+  } else if (this.size === "medium") {
+    return ("11")
+  } else if (this.size === "large") {
+    return ("13")
   }
-
 };
-
-
 
 //ui logic
 $(document).ready(function() {
-  $("button#order-now").submit(function() {
-    var size = $("input:radio[name=size]:checked").val();
-    var topping1 = $("input:radio[name=topping1]:checked").val();
-    var topping2 = $("input:radio[name=topping2]:checked").val();
-    var topping3 = $("input:radio[name=topping3]:checked").val();
-    var topping4 = $("input:radio[name=topping4]:checked").val();
+  $("button#order").click(function() {
 
+    var inputtedSize = $("input:radio[name=size]:checked").val();
+    var inputtedTopping = $("input:radio[name=topping1]:checked").val();
+    alert(inputtedSize);
+
+    var newPizza = new Pizza(inputtedSize, inputtedTopping);
+    alert(newPizza);
+
+    $("li#price").append("<li>" + Pizza.price() + "</li>");
+
+    // newPizza.price();
+    // $("li#price").text(this.size);
+    // var topping2 = $("input:radio[name=topping2]:checked").val();
+    // var topping3 = $("input:radio[name=topping3]:checked").val();
+    // var topping4 = $("input:radio[name=topping4]:checked").val()
 
   });
 });

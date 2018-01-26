be39a38e911b6f39dd88715c9c8962eb56a8be55
Maggie Summers <summers.maggie@gmail.com>
2018-01-26 10:18:56 -0800

    add js constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
 //biz logic
+function Pizza(size) {
+  this.size = size;
+  this.topping = [];
+};
 
-//ui logic 
+Pizza.prototype.price = function () {
+  if (this.size === small) {
+    return (this.size + (2 * this.topping));
+  }
+
+};
+
+
+
+//ui logic
+$(document).ready(function() {
+  $("button#order-now").submit(function() {
+    var size = $("input:radio[name=size]:checked").val();
+    var topping1 = $("input:radio[name=topping1]:checked").val();
+    var topping2 = $("input:radio[name=topping2]:checked").val();
+    var topping3 = $("input:radio[name=topping3]:checked").val();
+    var topping4 = $("input:radio[name=topping4]:checked").val();
+
+
+  });
+});

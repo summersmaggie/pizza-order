413b2051b2b782664f96c3888a0881ab134428ba
Maggie Summers <summers.maggie@gmail.com>
2018-01-26 12:05:29 -0800

    add js functionality to show price of pizza with size and two toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 //biz logic
-function Pizza(size, topping) {
+function Pizza(size, topping1, topping2) {
   this.size = size;
-  this.topping = topping;
+  this.topping1 = topping1;
+  this.topping2 = topping2;
 };
 
 Pizza.prototype.price = function() {
-  if (this.size === "small" && this.topping === "salami") {
-    return (7 + 4);
-  } else if (this.size === "medium" && this.topping === "jalepeño") {
-    return ("13")
-  } else if (this.size === "large") {
-    return ("16")
+  if (this.size === "small" && this.topping1 !== "none" && this.topping2 === "none") {
+    return (7 + 2 + 0);
+  } else if (this.size === "small" && this.topping1 === "none" && this.topping2 === "none") {
+    return (7 + 2 + 2);
+  } else if (this.size === "medium" && this.topping1 !== "none" && this.topping2 === "none") {
+    return (9 + 2 + 0);
+  } else if (this.size === "medium" && this.topping1 === "none" && this.topping2 === "none") {
+    return (9 + 2 + 2);
+  } else if (this.size === "large" && this.topping1 !== "none" && this.topping2 === "none") {
+    return (11 + 2 + 0)
+  } else if (this.size === "large" && this.topping1 === "none" && this.topping2 === "none") {
+    return (11 + 2 + 2)
   } else {
-    return ("7");
+    return ("Please try again.");
   }
 };
 
@@ -21,10 +28,10 @@ $(document).ready(function() {
   $("button#order").click(function() {
 
     var inputtedSize = $("input:radio[name=size]:checked").val();
-    var inputtedTopping = $("input:radio[name=topping1]:checked").val();
-    alert(inputtedTopping);
+    var inputtedTopping1 = $("input:radio[name=topping1]:checked").val();
+    var inputtedTopping2 = $("input:radio[name=topping2]:checked").val();
 
-    var pizzaOne = new Pizza(inputtedSize, inputtedTopping);
+    var pizzaOne = new Pizza(inputtedSize, inputtedTopping1, inputtedTopping2);
 
     $("#price").text(pizzaOne.price());
 
